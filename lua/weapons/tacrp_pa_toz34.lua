840e947c26bee7847d4a711f0cdd660373fcbbf9
TheOnly8Z <888888zombies@gmail.com>
2024-05-18 09:06:39 -0700

    TOZ34 vm rework

--- lua/weapons/tacrp_pa_toz34.lua
+++ lua/weapons/tacrp_pa_toz34.lua
@@ -11,7 +11,7 @@ SWEP.Category = "Tactical RP"
 SWEP.SubCatTier = "5Value"
 SWEP.SubCatType = "5Shotgun"
 
-SWEP.Description = "Double-barrel hunting shotgun. Slow reload time and poor ranged performance make this weapon more suited for hunting ducks than for combat."
+SWEP.Description = "Double-barrel hunting shotgun. Has decent accuracy and lethality, but its bulk and slow reload makes it ill-suited for combat."
 SWEP.Description_Quote = "\"A nu, chiki briki i v damki!\""
 
 SWEP.Trivia_Caliber = "12 Gauge"
@@ -19,9 +19,9 @@ SWEP.Trivia_Manufacturer = "Tulsky Oruzheiny Zavod"
 SWEP.Trivia_Year = "1964"
 
 SWEP.Faction = TacRP.FACTION_NEUTRAL
-SWEP.Credits = "Model: SAM61 \nSound: Navaro & Vunsunta \nAnimations: speedonerd & 8Z"
+SWEP.Credits = "Model: SAM61\nSound: rzen1th\nAnimations: speedonerd & 8Z"
 
-SWEP.ViewModel = "models/weapons/tacint/v_toz34.mdl"
+SWEP.ViewModel = "models/weapons/tacint_extras/v_toz34.mdl"
 SWEP.WorldModel = "models/weapons/tacint/w_toz34.mdl"
 
 SWEP.Slot = 2
@@ -29,54 +29,23 @@ SWEP.SlotAlt = 3
 
 SWEP.BalanceStats = {
     [TacRP.BALANCE_SBOX] = {
-        Damage_Max = 18,
+        Damage_Max = 16,
         Damage_Min = 10,
+        RPM = 130,
     },
     [TacRP.BALANCE_TTT] = {
-
-        Damage_Max = 8,
-        Damage_Min = 4,
-
-        Range_Min = 300,
-        Range_Max = 2000,
-
-        BodyDamageMultipliers = {
-            [HITGROUP_HEAD] = 2.5,
-            [HITGROUP_CHEST] = 1,
-            [HITGROUP_STOMACH] = 1,
-            [HITGROUP_LEFTARM] = 1,
-            [HITGROUP_RIGHTARM] = 1,
-            [HITGROUP_LEFTLEG] = 0.75,
-            [HITGROUP_RIGHTLEG] = 0.75,
-            [HITGROUP_GEAR] = 0.9
-        },
-    },
-    [TacRP.BALANCE_PVE] = {
-
-        Damage_Max = 10,
-        Damage_Min = 5,
-
-        BodyDamageMultipliers = {
-            [HITGROUP_HEAD] = 1.5,
-            [HITGROUP_CHEST] = 1,
-            [HITGROUP_STOMACH] = 1,
-            [HITGROUP_LEFTARM] = 0.75,
-            [HITGROUP_RIGHTARM] = 0.75,
-            [HITGROUP_LEFTLEG] = 0.5,
-            [HITGROUP_RIGHTLEG] = 0.5,
-            [HITGROUP_GEAR] = 0.9
-        },
+        // TODO
     },
 }
 
-SWEP.TTTReplace = TacRP.TTTReplacePreset.Pistol
+SWEP.TTTReplace = TacRP.TTTReplacePreset.Shotgun
 
 // "ballistics"
 
 SWEP.Damage_Max = 14
 SWEP.Damage_Min = 8
 SWEP.Range_Min = 500
-SWEP.Range_Max = 3500
+SWEP.Range_Max = 2800
 SWEP.Penetration = 1
 SWEP.ArmorPenetration = 0.45
 
@@ -85,8 +54,8 @@ SWEP.Num = 8
 SWEP.MuzzleVelocity = 10500
 
 SWEP.BodyDamageMultipliers = {
-    [HITGROUP_HEAD] = 2,
-    [HITGROUP_CHEST] = 1.2,
+    [HITGROUP_HEAD] = 1.5,
+    [HITGROUP_CHEST] = 1,
     [HITGROUP_STOMACH] = 1,
     [HITGROUP_LEFTARM] = 1,
     [HITGROUP_RIGHTARM] = 1,
@@ -95,17 +64,16 @@ SWEP.BodyDamageMultipliers = {
     [HITGROUP_GEAR] = 0.9
 }
 
-
 // misc. shooting
 
 SWEP.Firemodes = {
     1
 }
 
-SWEP.RPM = 125
+SWEP.RPM = 100
 
-SWEP.Spread = 0.02
-SWEP.ShotgunPelletSpread = 0.015
+SWEP.Spread = 0.015
+SWEP.ShotgunPelletSpread = 0.012
 
 SWEP.HipFireSpreadPenalty = 0.01
 SWEP.MidAirSpreadPenalty = 0
@@ -113,13 +81,14 @@ SWEP.MidAirSpreadPenalty = 0
 SWEP.ShootTimeMult = 0.75
 
 SWEP.RecoilPerShot = 1
-SWEP.RecoilMaximum = 2
+SWEP.RecoilMaximum = 1
 SWEP.RecoilResetTime = 0.3
-SWEP.RecoilDissipationRate = 5
+SWEP.RecoilDissipationRate = 2
 SWEP.RecoilFirstShotMult = 1
 
 SWEP.RecoilVisualKick = 3
 SWEP.RecoilKick = 12
+SWEP.RecoilStability = 0.6
 SWEP.RecoilAltMultiplier = 200
 
 SWEP.RecoilSpreadPenalty = 0.02
@@ -128,19 +97,19 @@ SWEP.CanBlindFire = true
 
 // handling
 
-SWEP.MoveSpeedMult = 0.9
-SWEP.ShootingSpeedMult = 0.75
-SWEP.SightedSpeedMult = 0.5
+SWEP.MoveSpeedMult = 0.85
+SWEP.ShootingSpeedMult = 0.5
+SWEP.SightedSpeedMult = 0.65
 
-SWEP.ReloadSpeedMult = 1.3
+SWEP.ReloadSpeedMult = 0.4
 
-SWEP.AimDownSightsTime = 0.4
-SWEP.SprintToFireTime = 0.4
+SWEP.AimDownSightsTime = 0.42
+SWEP.SprintToFireTime = 0.44
 
 SWEP.Sway = 1.5
-SWEP.ScopedSway = 0.25
+SWEP.ScopedSway = 0.1
 
-SWEP.FreeAimMaxAngle = 7.5
+SWEP.FreeAimMaxAngle = 6
 
 // hold types
 
@@ -163,19 +132,16 @@ SWEP.BlindFireSuicidePos = Vector(-7, 30, -25)
 SWEP.SprintAng = Angle(45, -10, 0)
 SWEP.SprintPos = Vector(6, 0, 0)
 
-SWEP.SightAng = Angle(0, 0, 0)
-SWEP.SightPos = Vector(-3.29, 0, 1.2)
-
-SWEP.CorrectiveAng = Angle(-0.2, 0, 0.5)
-SWEP.CorrectivePos = Vector(-0.025, 0, 0.15)
+SWEP.SightAng = Angle(0, 0.4, 0)
+SWEP.SightPos = Vector(-3.2, 0, 1.6)
 
 SWEP.CustomizeAng = Angle(30, 15, 0)
 SWEP.CustomizePos = Vector(5, 0, -2)
 
 SWEP.HolsterVisible = true
-SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_PISTOL
-SWEP.HolsterPos = Vector(0, 3, -4)
-SWEP.HolsterAng = Angle(90, 0, 0)
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_BACK2
+SWEP.HolsterPos = Vector(5, 0, -6)
+SWEP.HolsterAng = Angle(0, 0, 0)
 
 
 // reload
@@ -183,15 +149,16 @@ SWEP.HolsterAng = Angle(90, 0, 0)
 SWEP.ClipSize = 2
 SWEP.Ammo = "buckshot"
 
-SWEP.ReloadUpInTime = 1.65
+SWEP.ReloadUpInTime = 1.85
 
-SWEP.ReloadTimeMult = 1.2
+SWEP.ReloadTimeMult = 1.3
 
 // sounds
 
-local path = "tacint_extras/db/"
+local path = "tacint_extras/toz34/"
+local path1 = "tacint_shark/nitrorifle/"
 
-SWEP.Sound_Shoot = "^" .. path .. "deagle-1.wav"
+SWEP.Sound_Shoot = "^" .. path .. "fire-01.ogg"
 
 SWEP.Vol_Shoot = 130
 SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
@@ -226,15 +193,15 @@ SWEP.EjectEffect = 0
 // attack1
 SWEP.AnimationTranslationTable = {
     ["deploy"] = "draw",
-    ["fire_iron"] = "fire1",
-    ["fire1"] = "fire2",
-    ["fire2"] = "fire2",
+    ["fire_iron"] = "fire_iron",
+    ["fire1"] = "fire1",
+    ["fire2"] = "fire1",
     ["blind_fire"] = "blind_fire1",
-    ["melee"] = "melee2",
+    ["melee"] = {"melee1", "melee2"},
     ["jam"] = "dryfire"
 }
 
-SWEP.DeployTimeMult = 2.25
+SWEP.DeployTimeMult = 1.75
 
 SWEP.LastShot = false
 
@@ -243,7 +210,7 @@ SWEP.LastShot = false
 SWEP.Attachments = {
     [1] = {
         PrintName = "Accessory",
-        Category = {"acc", "acc_holster"},
+        Category = {"acc", "acc_sling"},
         AttachSound = "TacRP/weapons/flashlight_on.wav",
         DetachSound = "TacRP/weapons/flashlight_off.wav",
     },
@@ -279,10 +246,10 @@ local function addshellsound(name, spath)
     })
 end
 
-addsound("tacint_db.magrelease", path .. "magrelease.wav")
-addsound("tacint_db.open", path .. "openeject.wav")
-addsound("tacint_db.close", path .. "close.wav")
-addsound("tacint_db.magout", path .. "insert1.wav")
-addsound("tacint_db.magin", path .. "insert2.wav")
-addsound("tacint_db.unholster", path .. "cloth.wav")
-addshellsound("tacint_db.shells", path .. "shells.wav")
+
+addsound("tacint_toz34.open", path .. "open.ogg")
+addsound("tacint_toz34.close", path .. "close.ogg")
+addsound("tacint_toz34.magin", path .. "grab.ogg")
+addsound("tacint_toz34.deploy", path1 .. "deploy.wav")
+addsound("tacint_toz34.holster", path1 .. "holster.wav")
+addshellsound("tacint_toz34.shells", path .. "eject.ogg")
