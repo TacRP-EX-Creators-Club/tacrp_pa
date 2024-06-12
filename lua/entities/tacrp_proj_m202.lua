eff03456f98c56b6f6b894769b2c8c1eaf1092a4
Haodong Mo <mohaodong@gmail.com>
2024-06-12 22:42:19 +1000

    M202 FLASH

---
+++ lua/entities/tacrp_proj_m202.lua
@@ -0,0 +1,115 @@
+AddCSLuaFile()
+
+ENT.Base                     = "tacrp_proj_base"
+ENT.PrintName                = "M202 Rocket"
+ENT.Spawnable                = false
+
+ENT.Model                    = "models/weapons/tacint/rocket_deployed.mdl"
+
+ENT.IsRocket = false // projectile has a booster and will not drop.
+
+ENT.InstantFuse = false // projectile is armed immediately after firing.
+ENT.RemoteFuse = false // allow this projectile to be triggered by remote detonator.
+ENT.ImpactFuse = true // projectile explodes on impact.
+
+ENT.ExplodeOnDamage = true
+ENT.ExplodeUnderwater = true
+
+ENT.Delay = 0
+ENT.SafetyFuse = 0.05
+
+ENT.AudioLoop = "TacRP/weapons/rpg7/rocket_flight-1.wav"
+
+ENT.SmokeTrail = true
+
+ENT.FlareColor = Color(255, 255, 255)
+
+function ENT:Impact(data, collider)
+    if self.SpawnTime + self.SafetyFuse > CurTime() and !self.NPCDamage then
+        local attacker = self.Attacker or self:GetOwner()
+        local ang = data.OurOldVelocity:Angle()
+        local fx = EffectData()
+        fx:SetOrigin(data.HitPos)
+        fx:SetNormal(-ang:Forward())
+        fx:SetAngles(-ang)
+        util.Effect("ManhackSparks", fx)
+
+        if IsValid(data.HitEntity) then
+            local dmginfo = DamageInfo()
+            dmginfo:SetAttacker(attacker)
+            dmginfo:SetInflictor(self)
+            dmginfo:SetDamageType(DMG_CRUSH + DMG_CLUB)
+            dmginfo:SetDamage(250 * (self.NPCDamage and 0.5 or 1))
+            dmginfo:SetDamageForce(data.OurOldVelocity * 25)
+            dmginfo:SetDamagePosition(data.HitPos)
+            data.HitEntity:TakeDamageInfo(dmginfo)
+        end
+
+        self:EmitSound("weapons/rpg/shotdown.wav", 80)
+
+        for i = 1, 4 do
+            local prop = ents.Create("prop_physics")
+            prop:SetPos(self:GetPos())
+            prop:SetAngles(self:GetAngles())
+            prop:SetModel("models/weapons/tacint/rpg7_shrapnel_p" .. i .. ".mdl")
+            prop:Spawn()
+            prop:GetPhysicsObject():SetVelocityInstantaneous(data.OurNewVelocity * 0.5 + VectorRand() * 75)
+            prop:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+
+            SafeRemoveEntityDelayed(prop, 3)
+        end
+
+        self:Remove()
+        return true
+    end
+end
+
+function ENT:Detonate()
+    local attacker = self.Attacker or self:GetOwner()
+
+    local mult = TacRP.ConVars["mult_damage_explosive"]:GetFloat()
+    local dmg = DamageInfo()
+    dmg:SetDamageType(DMG_BURN)
+    dmg:SetDamage(60 * mult)
+    dmg:SetDamagePosition(self:GetPos())
+    dmg:SetInflictor(self)
+    dmg:SetAttacker(attacker)
+    util.BlastDamageInfo(dmg, self:GetPos(), 350)
+
+    self:FireBullets({
+        Attacker = attacker,
+        Damage = 100 * mult,
+        Tracer = 0,
+        Src = self:GetPos(),
+        Dir = self:GetForward(),
+        HullSize = 0,
+        Distance = 96,
+        IgnoreEntity = self,
+        Callback = function(atk, btr, dmginfo)
+            dmginfo:SetDamageType(DMG_AIRBOAT + DMG_BLAST) // airboat damage for helicopters and LVS vehicles
+            dmginfo:SetDamageForce(self:GetForward() * 700) // LVS uses this to calculate penetration!
+            util.Decal("Scorch", btr.StartPos, btr.HitPos - (btr.HitNormal * 16), self)
+        end,
+    })
+
+    local fx = EffectData()
+    fx:SetOrigin(self:GetPos())
+
+    if self:WaterLevel() > 0 then
+        util.Effect("WaterSurfaceExplosion", fx)
+    else
+        util.Effect("tacrp_m202_explode", fx)
+        self:EmitSound("^weapons/explode5.wav", 100, 110)
+        self:EmitSound("^ambient/fire/ignite.wav", 110, 110)
+    end
+
+    for _, ent in pairs(ents.FindInSphere(self:GetPos(), 350)) do
+        local tr = util.QuickTrace(self:GetPos(), ent:GetPos() - self:GetPos(), {self, ent})
+        if tr.Fraction == 1 then
+            // Ignite based on distance
+            ent:Ignite(9 * (math.Clamp(122500 - ent:GetPos():DistToSqr(self:GetPos()), 0, 122500) / 122500), 0)
+        end
+    end
+
+    self:Remove()
+end
---
+++ lua/weapons/tacrp_pa_m202.lua
@@ -0,0 +1,219 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "M202 FLASH"
+SWEP.Category = "Tactical RP (Special)"
+
+SWEP.SubCatTier = "9Special"
+SWEP.SubCatType = "6Launcher"
+
+SWEP.Description = "Incendiary rocket launcher. Fires four thermobaric rockets in quick succession."
+SWEP.Description_Quote = "\"Fire and forget!\""
+
+SWEP.Trivia_Caliber = "66mm Rockets"
+SWEP.Trivia_Manufacturer = "Northrop Electro-Mechanical Division"
+SWEP.Trivia_Year = "1978"
+
+SWEP.Faction = TacRP.FACTION_MILITIA
+SWEP.Credits = "Assets: Tactical Intervention"
+
+SWEP.ViewModel = "models/weapons/tacint/v_m202.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_m202.mdl"
+
+SWEP.NoRanger = true
+
+SWEP.Slot = 2
+SWEP.SlotAlt = 4
+
+SWEP.BalanceStats = {
+    [TacRP.BALANCE_SBOX] = {
+    },
+    [TacRP.BALANCE_TTT] = {
+    },
+}
+
+// "ballistics"
+
+SWEP.Damage_Max = 60 // just to fool the ratings
+SWEP.Damage_Min = 60
+SWEP.Range_Max = 5000
+SWEP.Range_Min = 3000
+
+SWEP.ShootEnt = "tacrp_proj_m202"
+SWEP.ShootEntForce = 7500
+
+SWEP.Num = 1
+
+// misc. shooting
+
+SWEP.Firemode = 2
+
+SWEP.RPM = 300
+
+SWEP.Spread = 0.03
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 1
+SWEP.RecoilResetTime = 0.2// time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 1
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 5
+SWEP.RecoilVisualShake = 0.2
+
+SWEP.RecoilKick = 0
+
+SWEP.RecoilSpreadPenalty = 0 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.75
+SWEP.ShootingSpeedMult = 0.25
+SWEP.SightedSpeedMult = 0.5
+
+SWEP.ReloadSpeedMult = 0.2
+
+SWEP.AimDownSightsTime = 0.95
+SWEP.SprintToFireTime = 0.95 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "rpg"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+SWEP.HoldTypeNPC = "ar2"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_RPG
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_AR2
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(4, 0, -4)
+
+SWEP.BlindFireAng = Angle(0, 0, 0)
+SWEP.BlindFirePos = Vector(1, -3, 2)
+
+SWEP.BlindFireLeftAng = Angle(75, 0, 0)
+SWEP.BlindFireLeftPos = Vector(8, 10, -6)
+
+SWEP.BlindFireRightAng = Angle(-75, 0, 0)
+SWEP.BlindFireRightPos = Vector(-8, 10, -12)
+
+SWEP.BlindFireSuicideAng = Angle(0, 125, 0)
+SWEP.BlindFireSuicidePos = Vector(-2, 25, -24)
+
+SWEP.SprintAng = Angle(40, -15, 0)
+SWEP.SprintPos = Vector(4, 0, -8)
+
+SWEP.SightAng = Angle(0, 0, 0)
+SWEP.SightPos = Vector(2.58, -6.5, -8.73)
+
+SWEP.HolsterVisible = true
+SWEP.HolsterSlot = TacRP.HOLSTER_SLOT_SPECIAL
+SWEP.HolsterPos = Vector(5, 0, -6)
+SWEP.HolsterAng = Angle(0, 0, 0)
+
+SWEP.CanBlindFire = false
+
+// sway
+
+SWEP.Sway = 3
+SWEP.ScopedSway = 0.25
+
+SWEP.FreeAimMaxAngle = 10
+
+// melee
+
+SWEP.CanMeleeAttack = false
+
+// reload
+
+SWEP.ClipSize = 4
+SWEP.Ammo = "rpg_round"
+
+// sounds
+
+local path = "TacRP/weapons/rpg7/"
+
+SWEP.Sound_Shoot = {
+    "^tacint_extras/m202/fire1.wav",
+    "^tacint_extras/m202/fire2.wav",
+    "^tacint_extras/m202/fire3.wav",
+    "^tacint_extras/m202/fire4.wav"
+}
+
+SWEP.Vol_Shoot = 130
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_1"
+
+// anims
+
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "draw",
+    ["blind_fire"] = "aimed_fire",
+    ["blind_idle"] = "aimed_idle",
+}
+
+// attachments
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Ammo",
+        Category = {"ammo_flash"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [2] = {
+        PrintName = "Accessory",
+        Category = {"acc", "acc_duffle", "acc_sling"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+    [3] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_shooting", "perk_reload"},
+        AttachSound = "tacrp/weapons/flashlight_on.wav",
+        DetachSound = "tacrp/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("TacInt_rpg7.jingle", path .. "handling-1.wav")
+addsound("TacInt_rpg7.insert_rocket", path .. "insert_rocket.wav")
+addsound("TacInt_rpg7.flipup_sight", path .. "flipup_sight.wav")
+
+if engine.ActiveGamemode() == "terrortown" then
+    SWEP.AutoSpawnable = false
+    SWEP.Kind = WEAPON_EQUIP
+    SWEP.Slot  = 6
+    SWEP.CanBuy = { ROLE_TRAITOR }
+    SWEP.LimitedStock = true
+    SWEP.EquipMenuData = {
+        type = "Weapon",
+        desc = "Rocket launcher. Can't explode at point blank.\nComes with 2 rockets.\n\nBEWARE: May be visible while holstered!",
+    }
+
+    function SWEP:TTTBought(buyer)
+        buyer:GiveAmmo(1, "RPG_Round")
+    end
+end
